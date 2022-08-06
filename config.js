41b6281bcae6285a5c77089d5146d51a1e273eb6
minella <phminella@gmail.com>
2022-08-07 07:41:22 +0900

    next js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `https://kapima.herokuapp.com/`;
-export const prodEndpoint = `https://kapima.herokuapp.com/`;
+export const endpoint = `https://kapima-backend.herokuapp.com/`;
+export const prodEndpoint = `https://kapima-backend.herokuapp.com/`;
