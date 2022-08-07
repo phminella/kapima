e9c777f915bcba92dba91a26c96aba750cc253c6
minella <phminella@gmail.com>
2022-08-07 17:32:13 +0900

    backend session

--- components/styles/GlobalStyles.js
+++ components/styles/GlobalStyles.js
@@ -47,10 +47,7 @@ body {
   margin:0;
   min-height:100vh!important;
   font-size:clamp(1.3rem, 2vw, 1.5rem);
-  background-color: greenyellow;
-  /* CSS gradients */
-  /* background-image: linear-gradient(transparent, white 65%), url(../img/index-bg.jpg); */
-  /* background-size: cover; */
-  /* background-repeat: no-repeat; */
+  background: rgb(247,20,95);
+background: linear-gradient(180deg, rgba(247,20,95,1) 0%, rgba(255,255,255,1) 100%);
 }
 `;
